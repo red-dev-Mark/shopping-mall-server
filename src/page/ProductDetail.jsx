4aa5034d7b728c8011c43ae248c505e9fe38ce56
Mark <redhero8830@gmail.com>
2024-03-29 00:17:36 +0900

    add product-detail page

--- src/page/ProductDetail.jsx
+++ src/page/ProductDetail.jsx
@@ -1,10 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from "react";
+import { Col, Container, Row } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 export default function ProductDetail() {
-    return (
-        <div>
-            상품상세페이지
-        </div>
-    );
-}
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
 
+  useEffect(() => {
+    const getProductDetail = async () => {
+      let url = new URL(`http://localhost:3004/products/${id}`);
+      const response = await fetch(url);
+      const data = await response.json();
+      // console.log(data);
+      setProduct(data);
+    };
+    getProductDetail();
+  }, [id]); //props로 { id }를 넘기거나, useParams()의 id를 갖고와도 의존성 불안정..
+
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  return (
+    <Container>
+      <Row>
+        <Col>
+          <img src={product?.img} alt="img" />
+        </Col>
+        <Col>
+          <h2>{product?.title}</h2>
+          <h3>{product?.price}</h3>
+          <h4>{product?.size}</h4>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
